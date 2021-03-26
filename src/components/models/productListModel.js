442f287e892a10c92886564575523f54637c0b2a
Raccoon <nobllizdc@gmail.com>
2021-03-27 01:56:08 +0900

    [Deploy]

--- src/components/models/productListModel.js
+++ src/components/models/productListModel.js
@@ -11,7 +11,7 @@ export default class ProductListModel extends ProductModel {
   }
 
   changeCount(data) {
-    console.log(data.id)
+    console.log(data)
     console.log(this.productList)
   }
 
